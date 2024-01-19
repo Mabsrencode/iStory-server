17d6d8abe7cd6dabda464a9421883ebbac53c549
Reniel George Mababa <mabsrencode@gmail.com>
2024-01-19 12:44:53 +0800

    server fixed

--- index.js
+++ index.js
@@ -2,28 +2,35 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.route.js";
+
 const app = express();
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const dbUrl = process.env.DB_URL;
 const PORT = process.env.PORT || 4000;
+
 const connection = mongoose.connection;
+
 try {
-  mongoose.connect(dbUrl);
-  connection.on("connected", () => {
-    console.log("Mongo db connection successful");
-  });
-  connection.on("error", () => {
-    console.log("Mongo db connection error");
-  });
+  mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 } catch (error) {
   console.log(error.message);
 }
+
+connection.once("open", () => {
+  console.log("MongoDB connection successful");
+});
+
+connection.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
 app.use((req, res, next) => {
   console.log(req.path, req.body);
   next();
